d908fd6eddf48536d75fc6762865d467c1a482a3
VitaliMay <mvt30@mail.ru>
2022-11-21 14:01:15 +0300

    feat: towel-sort solution v1 and v2

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  // Решение версия 1 ******************************************************************/
+
+  if (matrix === undefined || matrix.length === 0) return [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i%2 !== 0 ) matrix[i] = matrix[i].reverse() // если нечётное, то разворачиваем
+  }
+
+  return matrix.flat();                             // раскрываем внутренние массивы
+
+
+
+  // Решение версия 2 ******************************************************************/
+
+  // let result = []
+  // if (matrix === undefined || matrix.length === 0) return result;
+  // for (let i = 0; i < matrix.length; i++) {
+  //   if (i%2 !== 0 ) matrix[i] = matrix[i].reverse()   // если нечётное, то разворачиваем
+  //   result = result.concat(matrix[i])                 // загоняем в результат сразу раскрывая
+  // }
+  // return result;
+
 }
